abf2db78e4679cd6db8e19510fb838315ec169d0
Hiroki Yoshioka <irohiroki@gmail.com>
2013-02-07 17:24:49 +0900

    インストール済みのときはgemをインストールしない

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,4 +8,5 @@
 #
 execute "install_bundler" do
   command "gem i bundler --no-ri --no-rdoc"
+  not_if { begin Gem::Specification.find_by_name('bundler'); rescue Gem::LoadError => e; false end }
 end
