d2070c60f65f5b13c513498a76412d20a0cca84c
Hiroki Yoshioka <irohiroki@gmail.com>
2013-02-08 18:03:24 +0900

    app-serverはntpに依存

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,6 +6,8 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+require_recipe 'ntp'
+
 execute "install_bundler" do
   command "gem i bundler --no-ri --no-rdoc"
   not_if { begin Gem::Specification.find_by_name('bundler'); rescue Gem::LoadError => e; false end }
