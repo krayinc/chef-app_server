e77c98a16b4f67eed6b0800615ca3c063728d2e6
Hiroki Yoshioka <irohiroki@gmail.com>
2013-07-19 13:10:58 +0900

    ::nginx_fcgi追加

---
+++ recipes/nginx_fcgi.rb
@@ -0,0 +1,28 @@
+package 'fcgi-devel'
+package 'spawn-fcgi'
+
+bash 'install_fcgiwrap' do
+  cwd '/usr/local/src'
+  code <<-END
+    git clone https://github.com/gnosek/fcgiwrap.git
+    cd fcgiwrap
+    autoreconf -i
+    ./configure
+    make
+    make install
+  END
+  not_if { File.exist? '/usr/local/sbin/fcgiwrap' }
+end
+
+template '/etc/sysconfig/spawn-fcgi'
+
+service 'spawn-fcgi' do
+  action [:enable, :start]
+end
+
+service 'nginx'
+
+template "#{node[:nginx][:dir]}/app-server-conf.d/fcgi.conf" do
+  mode 00644
+  notifies :reload, 'service[nginx]'
+end
