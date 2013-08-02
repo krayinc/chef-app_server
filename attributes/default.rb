5a538930abc1475ea044dc76d32b34ce920bee41
Hiroki Yoshioka <irohiroki@gmail.com>
2013-08-02 17:15:48 +0900

    default attributesをいくつか設定

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,10 @@
+deploy_to = "/var/www/#{node.name}/#{node.chef_environment}"
+
+default[:app_server][:log_path] = "#{deploy_to}/shared/log"
+default[:app_server][:root]     = "#{deploy_to}/current/public"
+default[:app_server][:server_name] = node.name
+default[:app_server][:sock] = "unix:/tmp/#{node.name}-#{node.chef_environment}.sock"
+
+default[:app_server][:capistrano_pre_setup][:deploy_to] = deploy_to
+
 default[:app_server][:ruby][:version] = '2.0.0-p247'
