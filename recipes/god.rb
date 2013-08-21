2316600d908e470bf8864888c87ce692a9030dc4
Hiroki Yoshioka <irohiroki@gmail.com>
2013-08-21 19:31:39 +0900

    godの設定ディレクトリ追加

--- recipes/god.rb
+++ recipes/god.rb
@@ -1,5 +1,10 @@
 rbenv_gem 'god'
 
+directory '/etc/god/conf.d' do
+  mode 00755
+  recursive true
+end
+
 template "/etc/god/conf.d/#{node.name}-#{node.chef_environment}.conf" do
   source 'app_monitor.erb'
   mode 00644
