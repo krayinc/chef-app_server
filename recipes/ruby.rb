03f166a15eabbdcde00e9ff081e540e9ac407d43
Hiroki Yoshioka <irohiroki@gmail.com>
2013-09-09 12:43:08 +0900

    複数のRubyをインストールできるように修正

--- recipes/ruby.rb
+++ recipes/ruby.rb
@@ -5,6 +5,14 @@ rbenv_ruby node[:app_server][:ruby][:version] do
   global true
 end
 
+if node[:app_server][:ruby][:versions]
+  node[:app_server][:ruby][:versions].each do |version|
+    rbenv_ruby version do
+      global true
+    end
+  end
+end
+
 (node[:app_server][:ruby][:gems] | ['bundler']).each do |gem|
   rbenv_gem gem
 end
