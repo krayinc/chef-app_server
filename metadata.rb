73c4fc00050c8b212c9af84430c8cae673ab79e0
Hiroki Yoshioka <irohiroki@gmail.com>
2014-03-10 10:56:35 +0900

    アクセス制限はhttp_x_forwarded_forで

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'all@kray.jp'
 license          'All rights reserved'
 description      'Installs/Configures app-server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.2.1'
 depends          'base-server'
 depends          'ntp'
 depends          'nginx'
